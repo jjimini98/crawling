0949de7da585cb10f56f3fe1da3e8383ccd5c8d8
jimin <jjimini98@naver.com>
2020-11-15 15:27:40 +0900

    proceed making sentiment dictionary

---
+++ hotel_review/result_afinn.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from afinn import Afinn
+
+
+#Afinn 패키지를 사용하면 바로 점수도 나오는듯 하지만 시간이 엄청 오래 걸린다 .
+#https://github.com/fnielsen/afinn
+
+
+afinn = Afinn()
+
+pos_file = open("pos.txt", "r")
+pos_line = pos_file.readlines()
+line_num = 1
+score = []
+for x in pos_line:
+    result = afinn.score(x)
+    score.append(result)
+pos_file.close()
+
+pos_result = open("pos_result.txt", "w")
+
+for x in pos_line:
+    for y in score:
+        pos_result.writelines(x)
+        pos_result.write(y)
+
+pos_result.close()
+
+
+
